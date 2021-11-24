e797ebaf52beb6fb02c00295ec56cc0b2bd18c8c
Maxim Suslov <suslov.mn@gmail.com>
2021-11-24 07:01:01 +0300

    Add solution partly
    - not all tests are exist but for change_dir

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
 #!/usr/bin/env python3
+import argparse
+import os
+
+import server.FileService as FileService
+
+
 def main():
-    pass
+    """Entry point of app.
+
+    Get and parse command line parameters and configure web app.
+
+    Command line options:
+    -f --folder - working directory (absolute or relative path, default: current app folder).
+    -h --help - help.
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f', '--folder', default=os.path.join(os.getcwd(), 'data'), type=str,
+                        help="working directory (default: 'data' folder)")
+
+    params = parser.parse_args()
+    FileService.change_dir(params.folder)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
--- server/FileService.py
+++ server/FileService.py
@@ -1,3 +1,15 @@
+import logging
+import os
+import re
+import shutil
+
+import utils.TimeUtils as TimeUtils
+
+
+def _is_unsafe_folder_name(path: str) -> bool:
+    # security check
+    return bool(re.search(r'(^|[\\/])\.\.($|[\\/])', path))
+
 
 def change_dir(path: str, autocreate: bool = True) -> None:
     """Change current directory of app.
@@ -11,7 +23,16 @@ def change_dir(path: str, autocreate: bool = True) -> None:
         ValueError: if path is invalid.
     """
 
-    pass
+    if _is_unsafe_folder_name(path):
+        raise ValueError('Incorrect value of folder: {}'.format(path))
+
+    if not os.path.exists(path):
+        if autocreate:
+            os.makedirs(path)
+        else:
+            raise RuntimeError('Directory {} is not found'.format(path))
+    os.chdir(path)
+    logging.debug('change working directory to %s', path)
 
 
 def get_files() -> list:
@@ -25,7 +46,54 @@ def get_files() -> list:
         - size (int): size of file in bytes.
     """
 
-    pass
+    path = os.getcwd()
+
+    # get list of files in `path`
+    files = []
+    for root, dirnames, filenames in os.walk(path):
+        for filename in filenames:
+            files.append(os.path.join(root, filename))
+
+    # collect information about each file
+    data = []
+    prefix_size = len(path) + 1
+    for full_filename in files:
+        filename = full_filename[prefix_size:]
+        data.append({
+            'name': filename,
+            'create_date': TimeUtils.floattime_to_datatime(os.path.getctime(full_filename)),
+            'edit_date': TimeUtils.floattime_to_datatime(os.path.getmtime(full_filename)),
+            'size': os.path.getsize(full_filename),
+        })
+
+    return data
+
+
+def _filename_to_local_path(filename: str, folder_autocreate: bool = False) -> str:
+    """Get local path for filename.
+
+    Args:
+        filename (str): Filename
+        folder_autocreate (bool): Create a subfolder if True
+
+    Returns:
+        (str) Local path.
+
+    Raises:
+        ValueError: if filename is invalid.
+    """
+
+    if _is_unsafe_folder_name(filename):
+        raise ValueError('Incorrect value of filename: {}'.format(filename))
+
+    path = os.getcwd()
+    full_filename = os.path.join(path, filename)
+
+    folder = os.path.dirname(full_filename)
+    if folder_autocreate:
+        os.makedirs(folder)
+
+    return full_filename
 
 
 def get_file_data(filename: str) -> dict:
@@ -47,7 +115,18 @@ def get_file_data(filename: str) -> dict:
         ValueError: if filename is invalid.
     """
 
-    pass
+    local_file = _filename_to_local_path(filename)
+    if not os.path.exists(local_file):
+        raise RuntimeError('File {} does not exist'.format(filename))
+
+    with open(local_file, 'rb') as file_handler:
+        return {
+            'name': filename,
+            'create_date': TimeUtils.floattime_to_datatime(os.path.getctime(local_file)),
+            'edit_date': TimeUtils.floattime_to_datatime(os.path.getmtime(local_file)),
+            'size': os.path.getsize(local_file),
+            'context': file_handler.read(),
+        }
 
 
 def create_file(filename: str, content: str = None) -> dict:
@@ -68,7 +147,22 @@ def create_file(filename: str, content: str = None) -> dict:
         ValueError: if filename is invalid.
     """
 
-    pass
+    local_file = _filename_to_local_path(filename)
+
+    if os.path.exists(local_file):
+        logging.warning('file %s exists', local_file)
+
+    with open(local_file, 'wb') as file_handler:
+        if content:
+            data = bytes(content)
+            file_handler.write(data)
+
+    return {
+        'name': filename,
+        'create_date': TimeUtils.floattime_to_datatime(os.path.getctime(local_file)),
+        'size': os.path.getsize(local_file),
+        'content': content,
+    }
 
 
 def delete_file(filename: str) -> None:
@@ -82,4 +176,11 @@ def delete_file(filename: str) -> None:
         ValueError: if filename is invalid.
     """
 
-    pass
+    local_file = _filename_to_local_path(filename)
+    if not os.path.exists(local_file):
+        raise RuntimeError('File {} does not exist'.format(filename))
+
+    if os.path.isdir(local_file):
+        shutil.rmtree(local_file)
+    else:
+        os.remove(local_file)
